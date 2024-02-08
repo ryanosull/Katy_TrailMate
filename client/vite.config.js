84633e7484a221a033c406210d8f5d81e54bbd0c
William Ryan OSullivan <wrosullivan@gmail.com>
2024-02-08 09:47:59 -0600

    vite setup, issues with proxy and server not force starting on 4000

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dns from 'dns';
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 4000,
+    proxy: {
+      '/foo': 'http://localhost:3000',
+      // '/api': {
+      //   target: 'http://localhost:3000',
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/api/, ''),
+      // },
+    },
   },
-
-})
+});
